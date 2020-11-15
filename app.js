3bdf3ec0d2efa8d94c486792fbf7f7a2abbe927b
Unchun Yang <ucyang@ucyang.com>
2020-11-15 21:08:11 +0900

    app.js에 모듈 로드 및 기초 설정 코드 추가

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
 var logger = require('morgan');
+var session = require('express-session');
+// var passport = require('passport');
+// var passportConfig = require('./passport');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var apiAuthRouter = require('./routes/apiAuth');
+// var apiV1Router = require('./routes/apiV1');
 
 var app = express();
 
@@ -13,14 +20,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Initialize middlewares
+app.use(cors({ origin: true, credentials: true }));
 app.use(logger('dev'));
+app.use(methodOverride());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser('cookieParser_secret_key'));
+app.use(session({ secret: 'session_secret_key', resave: false, saveUninitialized: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/api/auth', apiAuthRouter);
+// app.use('/api/v1', apiV1Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
