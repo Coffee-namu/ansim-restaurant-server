f85f276e970fba5d8767dc5b63c7b6dc0a232d11
Unchun Yang <ucyang@ucyang.com>
2020-11-23 21:45:08 +0900

    V1 API 소스에 bcrypt 모듈 로드 코드 추가

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 // var models = require('../models');
 
 router.get('/test', function(req, res, next) {
