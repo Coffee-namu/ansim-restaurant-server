fc1f42d8eb39f6bf6a93d93abea108b26f7637cf
Unchun Yang <ucyang@ucyang.com>
2020-11-26 12:53:10 +0900

    임시 테스트 코드 제거 및 회원 정보 처리 REST API 추가

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -1,10 +1,64 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
-// var models = require('../models');
+var models = require('../models');
 
-router.get('/test', function(req, res, next) {
-  res.json({ test: 'test' });
+router.post('/members', function(req, res, next) {
+  req.body.password = bcrypt.hashSync(req.body.password, 10);
+  models.member.create(req.body, {
+    fields: ['username', 'password', 'is_owner', 'is_customer'],
+  })
+  .then(() => res.status(201).end())
+  .catch(() => res.status(400).end());
+});
+
+router.get('/members', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.json({
+      member_id: req.user.member_id,
+      username: req.user.username,
+      created: req.user.created,
+      is_admin: Boolean(req.user.is_admin),
+      is_owner: Boolean(req.user.is_owner),
+      is_customer: Boolean(req.user.is_customer),
+    });
+  } else {
+    res.status(401).end();
+  }
+});
+
+router.put('/members', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    models.member.update(req.body, {
+      fields: ['password'],
+      where: {
+        member_id: req.user.member_id,
+      },
+    })
+    .then(() => res.end())
+    .catch(() => res.status(400).end());
+  } else {
+    res.status(401).end();
+  }
+});
+
+router.get('/members/:member_id', function(req, res, next) {
+  models.member.findByPk(req.params.member_id, {
+    attributes: ['member_id', 'username', 'created',
+      'is_admin', 'is_owner', 'is_customer'],
+  })
+  .then(member => {
+    if (member) {
+      member.is_admin = Boolean(member.is_admin);
+      member.is_owner = Boolean(member.is_owner);
+      member.is_customer = Boolean(member.is_customer);
+      res.json(member);
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch(() => res.status(400).end());
 });
 
 module.exports = router;
