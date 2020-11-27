c8bd765eb6da737c6bf724c692b17d0d3987b9d6
kze <esimileiar81@gmail.com>
2020-11-27 22:55:51 +0900

    가장 가까운 레스토랑 추천/ 게시판 기능 기본/ 리뷰 기능 기본 추가

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -61,4 +61,66 @@ router.get('/members/:member_id', function(req, res, next) {
   .catch(() => res.status(400).end());
 });
 
+//Sequelize operator
+const {Op} = require("sequelize");
+
+//Get Ansim Restaurant Data Array(JSON)
+router.get('/ansim',function(req, res, next){
+  //query around 5km restaurant
+  models.restaurant.findAll({
+    where:{
+      [Op.and]: [
+        {
+          x:{
+            [Op.between]:[req.body.x-0.004545, req.body.x+0.004545]
+          }
+        },
+        {
+          y:{
+          [Op.between]:[req.body.y-0.005681, req.body.y+0.005681]
+          }
+        }
+      ]
+    }
+  }).then((result) =>{
+      //get distance
+      var distanceSet=[];
+      for(var i=0;i<result.length;i++){
+        distanceSet.push(pythagorasFormula(req.body.x,req.body.y,result[i].x, result[i].y))
+      }
+      //set rank
+      var rankArray=[];
+      for(var j=0;j<distanceSet.length;j++){
+        var rank = 1;
+        for(k=0;k<distanceSet.length;k++){
+          if(distanceSet[j]<distanceSet[k]){
+            rank++;
+          }
+        }
+        rankArray.push(rank);
+      }
+      //
+      var pushResult=[];
+      for(var find=0;find<rankArray.length;find++){
+        pushResult.push(result[rankArray.indexOf(find+1)]);
+      }
+      res.json(pushResult);
+    }
+  )
+  .catch(()=> res.status(401).end());
+});
+function pythagorasFormula(userx, usery, restx, resty){
+  //Special Law of Cosines.
+  //IEEE 754, 64bit calculation.
+  //error is about 5M~.
+  const R = 6371e3;
+  const pi1 = userx*Math.PI/180;
+  const pi2 = restx*Math.PI/180;
+  const rad = (usery-resty)*Math.PI/180;
+  const dist = Math.acos(Math.sin(pi1)*Math.sin(pi2)+Math.cos(pi1)*Math.cos(pi2)*Math.cos(rad))*R;
+  return dist;
+}
+
+
+//
 module.exports = router;
